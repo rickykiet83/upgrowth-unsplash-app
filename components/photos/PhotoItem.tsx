84d0426f62cbc1965e87d9f3ebbe0c2b8be68e33
Kiet Pham <kietpham.dev@gmail.com>
2021-09-26 21:06:32 +1000

    add photo to favorites

--- components/photos/PhotoItem.tsx
+++ components/photos/PhotoItem.tsx
@@ -1,11 +1,28 @@
+import React, { useContext } from 'react';
+
+import FavoritesContext from '@store/favorites-context';
 import Image from 'next/image';
 import { Photo } from '@models/photo.model';
-import React from 'react';
 
 export default function PhotoItem({ photo }: { photo: Photo }) {
+  const favoritesCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(photo.id);
+
+  function toggleFavoritesStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(photo.id);
+    } else {
+      favoritesCtx.addFavorite(photo);
+    }
+  }
+
   return (
     <li className='m-2'>
-      <img src={photo.urls.regular} alt={photo.alt_description} />
+      <img
+        onClick={toggleFavoritesStatusHandler}
+        src={photo.urls.regular}
+        alt={photo.alt_description}
+      />
     </li>
   );
 }
